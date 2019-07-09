e9fa9bad416a28b71ef0d5c66e913453d09f22d0
Craig A Wiswell <cwiswell63@gmail.com>
2019-07-08 20:51:23 -0400

    Add lights to background image panel

--- src/pokedex-main-panel/pokedex-main-panel.tsx
+++ src/pokedex-main-panel/pokedex-main-panel.tsx
@@ -7,8 +7,10 @@ const PokedexMainPanel: React.FC = () => {
     return (
         <div className="mainPanel">
             <div className="backgroundImagePanel">
+                <div className="leftLight imagePanelLight"></div>
+                <div className="rightLight imagePanelLight"></div>
                 <div className="spritePanel">
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" alt="sprite image" width="100%"/>
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" alt="pokemon sprite" width="100%"/>
                 </div>
             </div>
         </div>
