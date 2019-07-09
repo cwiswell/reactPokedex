9f22b5ebcfecd3e58ac2eecb9218f5a71c7cd1a0
Craig A Wiswell <cwiswell63@gmail.com>
2019-07-08 20:37:43 -0400

    Add placeholder sprite image

--- src/pokedex-main-panel/pokedex-main-panel.tsx
+++ src/pokedex-main-panel/pokedex-main-panel.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './pokedex-main-panel.css';
 
+// replace the src with data from api
+
 const PokedexMainPanel: React.FC = () => {
     return (
         <div className="mainPanel">
             <div className="backgroundImagePanel">
                 <div className="spritePanel">
-
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" alt="sprite image" width="100%"/>
                 </div>
             </div>
         </div>
