07d370b3d55bc45133f870e35e091283c87e84a9
Craig A Wiswell <cwiswell63@gmail.com>
2019-07-09 21:18:51 -0400

    Add css corner cut on image panel

--- src/pokedex-main-panel/pokedex-main-panel.tsx
+++ src/pokedex-main-panel/pokedex-main-panel.tsx
@@ -4,18 +4,22 @@ import './pokedex-main-panel.css';
 // replace the src with data from api
 
 type PokedexMainPanelProp = {
-    spriteUrl: string 
+    spriteUrl: string
 }
 
 const PokedexMainPanel: React.FC<PokedexMainPanelProp> = (props) => {
     return (
         <div className="mainPanel">
-            <div className="backgroundImagePanel">
-                <div className="leftLight imagePanelLight"></div>
-                <div className="rightLight imagePanelLight"></div>
-                <div className="spritePanel">
-                    <img src={props.spriteUrl} alt="pokemon sprite" width="100%"/>
+            <div className="backgroundImagePanelBorder">
+                <div className="backgroundImagePanel">
+                    <div className="leftLight imagePanelSmallLight"></div>
+                    <div className="rightLight imagePanelSmallLight"></div>
+                    <div className="spritePanel">
+                        <img src={props.spriteUrl} alt="pokemon sprite" width="100%" />
+                    </div>
+                    <div className="imagePanelBottomLight"></div>
                 </div>
+
             </div>
         </div>
     )
