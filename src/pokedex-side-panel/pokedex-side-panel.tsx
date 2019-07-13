fbdbfea12d041f374f784959a6a8d8efdfd53fad
Craig A Wiswell <cwiswell63@gmail.com>
2019-07-12 20:19:20 -0400

    Add pokemon information

--- src/pokedex-side-panel/pokedex-side-panel.tsx
+++ src/pokedex-side-panel/pokedex-side-panel.tsx
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import './pokedex-side-panel.css';
 
 type PokedexSidePanelState = {
-
+  height: string | null;
+  weight: string | null;
+  description: string | null;
 }
 
 class PokedexSidePanel extends Component<any, PokedexSidePanelState> {
@@ -10,7 +12,9 @@ class PokedexSidePanel extends Component<any, PokedexSidePanelState> {
   render() {
     return (
         <div className="pokedexSidePanel">
-          <div className="descriptionArea"></div>
+          <div className="descriptionArea">
+            <div></div>
+          </div>
         </div>
     );
   };
