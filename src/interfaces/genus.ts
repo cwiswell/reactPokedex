91b7d44e35a63d99c060a2e807fd8835eb7a148b
Craig A Wiswell <cwiswell63@gmail.com>
2019-07-30 07:17:11 -0400

    Add genus to species interface

---
+++ src/interfaces/genus.ts
@@ -0,0 +1,6 @@
+import ReferenceItem from "./referenceItem";
+
+export default interface Genus{
+    genus: string,
+    language: ReferenceItem;
+}
--- src/interfaces/species.ts
+++ src/interfaces/species.ts
@@ -1,8 +1,22 @@
 import ReferenceItem from "./referenceItem";
+import Genus from "./genus";
 
 export default interface Species{
     base_happiness: number;
     capture_rate: number;
     color: ReferenceItem;
     egg_groups: Array<ReferenceItem>;
+    evolution_chain: ReferenceItem;
+    evolves_from_species: ReferenceItem;
+    flavor_text_entries: Array<any>;
+    form_descriptions: Array<any>;
+    forms_switchable: boolean;
+    gender_rate: number;
+
+    genera: Array<Genus>;
+    generation: ReferenceItem;
+    growth_rate: ReferenceItem;
+    habitat: ReferenceItem;
+
+    has_gender_differences: boolean;
 }
