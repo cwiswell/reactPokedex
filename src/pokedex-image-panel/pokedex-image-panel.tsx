4847f176ff28ab5e7b1937599071970861a3f42e
Craig A Wiswell <cwiswell63@gmail.com>
2019-08-04 13:29:04 -0400

    Add dictionary of sprite attributes

--- src/pokedex-image-panel/pokedex-image-panel.tsx
+++ src/pokedex-image-panel/pokedex-image-panel.tsx
@@ -2,10 +2,27 @@ import React, { Fragment } from 'react';
 import './pokedex-image-panel.css';
 import SpriteUrl from '../interfaces/spriteUrls';
 
+interface Sprite {
+    name: string;
+    index: string;
+}
+
+const spriteList: Array<Sprite> = [
+    {name: "Front Male", index: "front_default" },
+    {name: "Back Male", index: "back_default" },
+    {name: "Front Female", index: "front_female" },
+    {name: "Back Female", index: "back_female" },
+    {name: "Front Shiny Male", index: "front_shiny" },
+    {name: "Back Shiny Male", index: "back_shiny" },
+    {name: "Front Shiny Female", index: "front_shiny_female" },
+    {name: "Back Shiny Female", index: "back_shiny_female" }
+];
+
 type PokedexImagePanelProp = {
     spriteUrl: SpriteUrl | null;
     errorText: string | null;
 }
+
 
 const PokedexImagePanel: React.FC<PokedexImagePanelProp> = (props) => {
     let imgArea =(<Fragment>{props.errorText}</Fragment>);
@@ -13,7 +30,7 @@ const PokedexImagePanel: React.FC<PokedexImagePanelProp> = (props) => {
 
     let tempSpriteUrl: SpriteUrl = props.spriteUrl == null ? {} as SpriteUrl : props.spriteUrl;
     if (tempSpriteUrl !== null || tempSpriteUrl !== undefined) {
-        let currentUrl = tempSpriteUrl.front_default;
+        let currentUrl = tempSpriteUrl["front_default"];
         if(currentUrl != null){
             imgArea = (<img src={currentUrl} alt="pokemon sprite" width="90%" />);
             hasImage = true;
